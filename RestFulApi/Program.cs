3c4599d0088199f48b359644192014b2cf273c9e
hossein <hossein.borghei81@gmail.com>
2022-03-05 22:21:19 +0330

    [Add]healthChecks(for database and the application)

--- RestFulApi/Program.cs
+++ RestFulApi/Program.cs
@@ -1,9 +1,12 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
 using RestFulApi.Repositories;
 using RestFulApi.Settings;
+using System.Net.Mime;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,26 +14,35 @@ var builder = WebApplication.CreateBuilder(args);
 
 // if saw a Guid type convert it to string
 BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+
 // if saw a DataTimeOffset type convert it to string
 BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 
 //Add MongoDb
+var mongoSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
 builder.Services.AddSingleton<IMongoClient>(ServiceProvider =>
 {
-    var settings=builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-    return new MongoClient(settings.ConnectionString);
+    return new MongoClient(mongoSettings.ConnectionString);
 });
 
 builder.Services.AddSingleton<IItemRepository, MongoDbRepository>();
 builder.Services.AddControllers(options =>
 {
-    options.SuppressAsyncSuffixInActionNames = false; 
+    options.SuppressAsyncSuffixInActionNames = false;
 });
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHealthChecks()
+    .AddMongoDb(
+        mongoSettings.ConnectionString,
+        name: "mongoDb",
+        timeout: TimeSpan.FromSeconds(3),
+        tags: new[] { "ready" }
+    );
 
 var app = builder.Build();
 
@@ -43,8 +55,41 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+
+    endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions
+    {
+        Predicate = (check) => check.Tags.Contains("ready"),
+        ResponseWriter = async (context, report) =>
+        {
+            var result = JsonSerializer.Serialize(
+                new
+                {
+                    status = report.Status.ToString(),
+                    checks = report.Entries.Select(entry => new
+                    {
+                        name = entry.Key,
+                        status = entry.Value.Status.ToString(),
+                        exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                        duration = entry.Value.Duration.ToString(),
+                    })
+                }
+            );
+
+            context.Response.ContentType = MediaTypeNames.Application.Json;
+            await context.Response.WriteAsync(result);
+        }
+    });
+    endpoints.MapHealthChecks("/health/live", new HealthCheckOptions
+    {
+        Predicate = (_) => false
+    });
+});
 
 app.Run();
